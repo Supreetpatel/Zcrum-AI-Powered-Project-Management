8cf970d906d2fbf2e7b23aa31f09b0a8797b4def
Supreet0 <supreetpatel2004@gmail.com>
2025-03-27 04:04:14 +0530

    Making some progress in the landing page and added the neon db for database.

--- app/page.js
+++ app/page.js
@@ -1,5 +1,77 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { BarChart, Calendar, ChevronRight, Layout } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
+const features = [
+  {
+    title: "Intuitive Kanban Boards",
+    description:
+      "Visualize your workflow and optimize team productivity with our easy-to-use Kanban boards.",
+    icon: Layout,
+  },
+  {
+    title: "Powerful Sprint Planning",
+    description:
+      "Plan and manage sprints effectively, ensuring your team stays focused on delivering value.",
+    icon: Calendar,
+  },
+  {
+    title: "Comprehensive Reporting",
+    description:
+      "Gain insights into your team's performance with detailed, customizable reports and analytics.",
+    icon: BarChart,
+  },
+];
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen">
+      <section className="container mx-auto py-20 text-center">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-extrabold gradient-title pb-6 flex flex-col">
+          Streamline Your Workflow <br />
+          <span className="flex mx-auto gap-3 sm:gap-4 items-center">
+            with{" "}
+            <Image
+              src={"/logo2.png"}
+              alt="logo"
+              width={400}
+              height={80}
+              className="h-14 sm:h-24 w-auto object-contain"
+            />
+          </span>
+        </h1>
+        <p className="text-xl text-gray-300 mb-10 max-w-3xl mx-auto">
+          Empower your team with our intuitive project management solution.
+        </p>
+        <Link href="/onboarding">
+          <Button size="lg" className="mr-4">
+            Get Started <ChevronRight size={18} />
+          </Button>
+        </Link>
+        <Link href="#features">
+          <Button size="lg" variant="outline" className="mr-4">
+            Learn More
+          </Button>
+        </Link>
+      </section>
+      <section>
+        <div>
+          <h3>Key Features</h3>
+          <div className="grid grid-col-1 lg:grid-cols-3 gap-8">
+            {features.map((feature, index) => {
+              return (
+                <Card key={index}>
+                  <CardContent>
+                    <feature.icon className="h-12 w-12 mb-4 text-blue-300" />
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
